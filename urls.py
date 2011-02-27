502681caf81d23ea606cf7d47f497e10a4aeedee
Jori Lallo <jtlallo@gmail.com>
2011-02-27 16:20:44 +0200

    Added better styles and urls for login&logout

--- urls.py
+++ urls.py
@@ -12,14 +12,8 @@ urlpatterns = patterns('',
     (r'^admin/', include(admin.site.urls)),
     
     # Login & logout
-    #url(r'^accounts/logout/$', 'django.contrib.auth.views.logout', {'next_page': '/'}, name="auth_logout"),
-    #url(r'^accounts/login/$', 'django.contrib.auth.views.login', {'template_name': 'main/login.html'}, name="auth_login"),
-    
-    # Password reset
-    #url(r'^accounts/password/reset/$', 'django.contrib.auth.views.password_reset', {'template_name': 'registration/password_reset.html', 'email_template_name': 'registration/password_reset_email.html'}),
-    #url(r'^accounts/password/reset/done/$', 'django.contrib.auth.views.password_reset_done', {'template_name': 'registration/password_reset_done.html'}),
-    #url(r'^accounts/password/reset/confirm/(?P<uidb36>[0-9A-Za-z]+)-(?P<token>.+)/$', 'django.contrib.auth.views.password_reset_confirm', {'template_name': 'registration/password_reset_confirm.html'}),
-    #url(r'^accounts/password/reset/complete/$', 'django.contrib.auth.views.password_reset_complete', {'template_name': 'registration/password_reset_complete.html',}),
+    url(r'^accounts/logout/$', 'django.contrib.auth.views.logout', {'next_page': '/'}, name="auth_logout"),
+    url(r'^accounts/login/$', 'django.contrib.auth.views.login', {'template_name': 'main/login.html'}, name="auth_login"),
     
     # Project apps
     (r'^', include('main.urls')),
